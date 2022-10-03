93db8029078faadf251525dedc502edf999e507c
AkanshaShivila <akansha.sabre@shivila.com>
2022-10-04 03:05:06 +0530

    Initial commit

---
+++ goods/cart/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'cart', views.CartViewSet)
+router.register(r'user', views.UserViewSet)
+
+#urlpatterns = [
+ #   path('api_auth/', include('restframework.urls', namespace='rest_framework')),
+#]
+urlpatterns = router.urls
